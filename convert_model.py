c0874783daa5ee8eaf5b25b77786d747851808be
MatrixUniverse238 <baniket238@gmail.com>
2026-03-13 01:22:47 +0530

    changing tf_model.py

--- convert_model.py
+++ convert_model.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 
+# load your model
 model = tf.keras.models.load_model("virus_model.h5", compile=False)
 
-# correct filename
-model.save_weights("virus.weights.h5")
+# export to TensorFlow SavedModel
+model.export("virus_model_saved")
 
-print("Weights saved successfully")
+print("Model exported successfully!")
--- tf_model.py
+++ tf_model.py
@@ -1,23 +1,29 @@
 import numpy as np
 import cv2
 import tensorflow as tf
-from tensorflow.keras.models import load_model
+import streamlit as st
 
-# force legacy keras compatibility
-tf.keras.utils.get_custom_objects()
 
-model = load_model("virus_model.h5", compile=False)
+@st.cache_resource
+def load_tf_model():
+    model = tf.saved_model.load("virus_model_saved")
+    return model
+
 
 def predict_tf(image):
+
+    model = load_tf_model()
+    infer = model.signatures["serving_default"]
 
     img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     img = cv2.resize(img, (150,150))
     img = img / 255.0
 
     img = np.expand_dims(img, axis=-1)
-    img = np.expand_dims(img, axis=0)
+    img = np.expand_dims(img, axis=0).astype("float32")
 
-    pred = model.predict(img, verbose=0)[0][0]
+    pred = infer(tf.constant(img))
+    pred = list(pred.values())[0].numpy()[0][0]
 
     if pred > 0.5:
         return "PNEUMONIA", float(pred)
