4ce1d89e6d9625821a6c93b68f82183987759a15
MatrixUniverse238 <baniket238@gmail.com>
2026-03-13 00:20:23 +0530

    fix tensorflow version for deployment

--- tf_model.py
+++ tf_model.py
@@ -1,24 +1,16 @@
 import numpy as np
 import cv2
-from tensorflow.keras.models import load_model
+import tensorflow as tf
 
-model = load_model("virus_model.h5", compile=False)
+model = tf.keras.models.load_model("virus_model.h5", compile=False)
 
 def predict_tf(image):
 
-    # convert to grayscale
     img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # correct resize
     img = cv2.resize(img, (150,150))
-
-    # normalize
     img = img / 255.0
 
-    # add channel dimension
     img = np.expand_dims(img, axis=-1)
-
-    # add batch dimension
     img = np.expand_dims(img, axis=0)
 
     pred = model.predict(img, verbose=0)[0][0]
